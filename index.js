32d33aed393eca3c058c448715a4434ee7776406
Hannah Robinson <hannah1robinson1@gmail.com>
2025-01-15 13:01:45 -0700

    finsihed most of class spa

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function render(state = store.home) {
       ${main(state)}
       ${footer()}
     `;
-    // console.log(process.env.OPEN_WEATHER_MAP_API_KEY);
-    router.updatePageLinks();
+  // console.log(process.env.OPEN_WEATHER_MAP_API_KEY);
+
 }
 
-render();
+
 router.hooks({
   // We pass in the `done` function to the before hook handler to allow the function to tell Navigo we are finished with the before hook.
   // The `match` parameter is the data that is passed from Navigo to the before hook handler with details about the route being accessed.
@@ -43,18 +43,18 @@ router.hooks({
               description: response.data.weather[0].main
             };
             done();
-        })
-        .catch((err) => {
-          console.log(err);
-          done();
-        });
-      break;
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        break;
       // Add a case for each view that needs data from an API
       case "pizza":
         console.log(process.env.OPEN_WEATHER_MAP_API_KEY);
         // New Axios get request utilizing already made environment variable
         axios
-        .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
           .then(response => {
             // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
             console.log("response", store.pizza.pizzas);
@@ -66,11 +66,11 @@ router.hooks({
             console.log("It puked", error);
             done();
           });
-          break;
-      default :
+        break;
+      default:
         // We must call done for all views so we include default for the views that don't have cases above.
         done();
-        // break is not needed since it is the last condition, if you move default higher in the stack then you should add the break statement.
+      // break is not needed since it is the last condition, if you move default higher in the stack then you should add the break statement.
     }
   },
   already: (match) => {
@@ -79,34 +79,79 @@ router.hooks({
     render(store[view]);
   },
   after: (match) => {
+    const view = match?.data?.view ? camelCase(match.data.view) : "home";
     router.updatePageLinks();
 
     // add menu toggle to bars icon in nav bar
     document.querySelector(".fa-bars").addEventListener("click", () => {
-        document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+      document.querySelector("nav > ul").classList.toggle("hidden--mobile");
     });
+
+    if (view === "order") {
+      // Add an event handler for the submit button on the form
+      document.querySelector("form").addEventListener("submit", event => {
+        event.preventDefault();
+    
+        // Get the form element
+        const inputList = event.target.elements;
+        console.log("Input Element List", inputList);
+    
+        // Create an empty array to hold the toppings
+        const toppings = [];
+    
+        // Iterate over the toppings array
+    
+        for (let input of inputList.toppings) {
+          // If the value of the checked attribute is true then add the value to the toppings array
+          if (input.checked) {
+            toppings.push(input.value);
+          }
+        }
+    
+        // Create a request body object to send to the API
+        const requestData = {
+          customer: inputList.customer.value,
+          crust: inputList.crust.value,
+          cheese: inputList.cheese.value,
+          sauce: inputList.sauce.value,
+          toppings: toppings
+        };
+        // Log the request body to the console
+        console.log("request Body", requestData);
+    
+        axios
+          // Make a POST request to the API to create a new pizza
+          .post(`${process.env.PIZZA_PLACE_API_URL}/pizzas`, requestData)
+          .then(response => {
+          //  Then push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+            store.pizza.pizzas.push(response.data);
+            router.navigate("/pizza");
+          })
+          // If there is an error log it to the console
+          .catch(error => {
+            console.log("It puked", error);
+          });
+      });
+    }
   }
 });
 
 router
-    .on({
-      "/": () => render(),
-    
-      ":view": (match) => {
-        // Change the :view data element to camel case and remove any dashes (support for multi-word views)
-        const view = match?.data?.view ? camelCase(match.data.view) : "home";
-        // Determine if the view name key exists in the store object
-        if (view in store) {
-          render(store[view]);
-        } else {
-          render(store.viewNotFound);
-          console.log(`View ${view} not defined`);
-        }
-      },
-    })
-    .resolve();
+  .on({
+    "/": () => render(), // handles http:/localhost:1234/
 
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+    ":view": (match) => {  // handles http:/localhost:1234/........... = /order, /home, /pizza
+      // Change the :view data element to camel case and remove any dashes (support for multi-word views)
+      const view = match?.data?.view ? camelCase(match.data.view) : "home";
+      // Determine if the view name key exists in the store object
+      if (view in store) {
+        render(store[view]);
+      } else {
+        render(store.viewNotFound);
+        console.log(`View ${view} not defined`);
+      }
+    },
+  })
+  .resolve();
+
+
